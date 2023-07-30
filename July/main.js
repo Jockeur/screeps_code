114f7bc5f94e929cf2c9607e08f6041c22cdb2b0
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-07-30 22:17:50 +0200

    LDA E16N5

--- July/main.js
+++ July/main.js
@@ -92,7 +92,7 @@ module.exports.loop = function () {
             spawn.spawnCustomCreep(energy, newName, 'repairer');
         } else if(longDistanceAttackers < maxLongDistanceAttackers){
             var newName = 'LDA' + Game.time;
-            spawn.spawnAttacker(energy, 5, newName, 'longDistanceAttacker', 'E17N6');
+            spawn.spawnAttacker(energy, 5, newName, 'longDistanceAttacker', 'E16N5');
         } else if (longDistanceHarvesters < maxLongDistanceHarvesters) {
             var newName = 'LDH' + Game.time;
             spawn.spawnLongDistanceCreep(energy, newName, 4, spawn.room.name, 'E17N6', 0, 'longDistanceHarvester');
