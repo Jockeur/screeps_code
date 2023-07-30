13b8d1de4140e067b7b08820b1d2037adf292582
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-07-30 22:20:21 +0200

    lorry ?

--- July/role.lorry.js
+++ July/role.lorry.js
@@ -1,19 +1,8 @@
 module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
-        // if creep is bringing energy to a structure but has no energy left
-        if (creep.memory.working == true && creep.carry.energy == 0) {
-            // switch state
-            creep.memory.working = false;
-        }
-        // if creep is harvesting energy but is full
-        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
-            // switch state
-            creep.memory.working = true;
-        }
-
         // if creep is supposed to transfer energy to a structure
-        if (creep.memory.working == true) {
+        if (creep.memory.working) {
             // find closest spawn, extension or tower which is not full
             var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
                 // the second argument for findClosestByPath is an object which takes
@@ -26,7 +15,7 @@ module.exports = {
             });
 
             // if we found one
-            if (structure != undefined) {
+            if (structure) {
                 // try to transfer energy, if it is not in range
                 if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     // move towards it
@@ -38,10 +27,11 @@ module.exports = {
         else {
             // find closest container
             let container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: s => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 0
+                filter: s => s.structureType == STRUCTURE_CONTAINER && s.energy > 0
             });
+            console.log(container)
             // if one was found
-            if (container != undefined) {
+            if (container) {
                 // try to withdraw energy, if the container is not in range
                 if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     // move towards it
