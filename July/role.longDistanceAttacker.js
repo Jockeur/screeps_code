81353c173e606b247167dcada942e93e080dd337
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-07-30 02:34:03 +0200

    test

--- July/role.longDistanceAttacker.js
+++ July/role.longDistanceAttacker.js
@@ -1,7 +1,7 @@
 var roleLongDistanceAttacker = {
     
     run:function(creep) {
-        if(creep.memory.target == creep.room.name) {
+        if(creep.room.name == creep.memory.target) {
             var closestHostileCreep = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
             var closestHostileStructure = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES);
             if(closestHostileCreep) {
@@ -10,6 +10,7 @@ var roleLongDistanceAttacker = {
                 creep.attack(closestHostileStructure);
             }
         } else {
+            console.log('going to ' + creep.memory.target)
             var exit = creep.room.findExitTo(creep.memory.target);
             creep.moveTo(creep.pos.findClosestByRange(exit), {visualizePathStyle: {stroke: '#0CFF00'}});
         }
