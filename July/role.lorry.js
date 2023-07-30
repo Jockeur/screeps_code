ee6d62ccf8048bb6c86b1a7216c3066988110c87
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-07-31 00:45:16 +0200

    less code

--- July/role.lorry.js
+++ July/role.lorry.js
@@ -1,27 +1,11 @@
+var roleHarvester = require('role.harvester');
+
 module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
         // if creep is supposed to transfer energy to a structure
         if (creep.memory.working) {
-            // find closest spawn, extension or tower which is not full
-            var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-                // the second argument for findClosestByPath is an object which takes
-                // a property called filter which can be a function
-                // we use the arrow operator to define it
-                filter: (s) => (s.structureType == STRUCTURE_SPAWN
-                             || s.structureType == STRUCTURE_EXTENSION
-                             || s.structureType == STRUCTURE_TOWER)
-                             && s.energy < s.energyCapacity
-            });
-
-            // if we found one
-            if (structure) {
-                // try to transfer energy, if it is not in range
-                if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    // move towards it
-                    creep.moveTo(structure);
-                }
-            }
+           roleHarvester.run(creep);
         }
         // if creep is supposed to get energy
         else {
