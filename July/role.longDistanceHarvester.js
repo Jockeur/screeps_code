b790338bc3c49e5d6d75b55ae075f34bf09a0928
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-08-01 17:03:50 +0200

    wrote LDH again

--- July/role.longDistanceHarvester.js
+++ July/role.longDistanceHarvester.js
@@ -1,49 +1,30 @@
 module.exports = {
-    // a function to run the logic for this role
     run: function(creep) {
-
-        // if creep is supposed to transfer energy to a structure
-        if (creep.memory.working) {
-            // if in home room
-            if (creep.room.name == creep.memory.home) {
-                // find closest spawn, extension or tower which is not full
-                var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-                    // the second argument for findClosestByPath is an object which takes
-                    // a property called filter which can be a function
-                    // we use the arrow operator to define it
-                    filter: (s) => (s.structureType == STRUCTURE_SPAWN
-                                 || s.structureType == STRUCTURE_EXTENSION
-                                 || s.structureType == STRUCTURE_TOWER)
-                                 && s.energy < s.energyCapacity
+        if(creep.memory.working) {
+            if(creep.room.name == creep.memory.home) {
+                var target = creep.pos.findClosestBypath(FIND_STRUCTURES, {filter: (s) => (
+                    s.structureType == STRUCTURE_SPAWN
+                    || s.structureType == STRUCTURE_EXTENSION
+                    || s.structureType == STRUCTURE_TOWER
+                    ) && s.energy < s.energyCapacity
                 });
-
-                // if we found one
-                if (structure) {
-                    // try to transfer energy, if it is not in range
-                    if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        // move towards it
-                        creep.moveTo(structure);
-                    }
+                
+                if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
                 }
-            }
-            // if not in home room...
-            else {
-                // find exit to home room
+            } else {
                 var exit = creep.room.findExitTo(creep.memory.home);
-                // and move to exit
-                creep.moveTo(creep.pos.findClosestByRange(exit));
+                creep.moveTo(creep.findClosestByPath(exit));
             }
-        }
-        else {
-            if (creep.room.name == creep.memory.target) {
+        } else {
+            if(creep.room.name == creep.memory.target) {
                 var source = creep.room.find(FIND_SOURCES)[creep.memory.sourceIndex];
-                if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(source);
                 }
-            }
-            else {
+            } else {
                 var exit = creep.room.findExitTo(creep.memory.target);
-                creep.moveTo(creep.pos.findClosestByRange(exit));
+                creep.moveTo(creep.findClosestByPath(exit));
             }
         }
     }
