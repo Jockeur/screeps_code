0545b6aa2d44d5574dbf4e1261339a63d120a030
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-08-01 20:32:34 +0200

    In the right room

--- July/main.js
+++ July/main.js
@@ -109,7 +109,7 @@ module.exports.loop = function () {
             spawn.spawnCustomCreep(energy, newName, 'wallRepairer')
         } else if (longDistanceAttackers < maxLongDistanceAttackers) {
             var newName = 'LDA' + Game.time;
-            spawn.spawnAttacker(energy, 5, newName, 'longDistanceAttacker', 'E16N5');
+            spawn.spawnAttacker(energy, 5, newName, 'longDistanceAttacker', 'E17N6');
         } else if (longDistanceHarvesters < maxLongDistanceHarvesters) {
             var newName = 'LDH' + Game.time;
             spawn.spawnLongDistanceCreep(energy, newName, 4, spawn.room.name, 'E17N6', 0, 'longDistanceHarvester');
