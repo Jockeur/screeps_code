6ae97b0fa8a7bede5367c7cf2c68262b10f2fddc
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-01 13:02:19 +0200

    fixed LDB

--- July/role.longDistanceBuilder.js
+++ July/role.longDistanceBuilder.js
@@ -7,7 +7,7 @@ var role = {
                 var container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                     filter: (s) => s.structureType == STRUCTURE_CONTAINER
                 })
-                if (creep.withdraw(container) == ERR_NOT_IN_RANGE) {
+                if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(container);
                 }
             } else {
