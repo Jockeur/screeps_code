b21c787eed77142a71db8536e76e88a2e3de67aa
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-01 16:09:18 +0200

    no container

--- July/role.builder.js
+++ July/role.builder.js
@@ -4,8 +4,15 @@ var roleBuilder = {
     run: function (creep) {
         if (!creep.memory.working) {
             var container = creep.pos.findClosestByPath(FIND_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_CONTAINER })
-            if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(container);
+            if (container) {
+                if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(container);
+                }
+            } else {
+                var source = creep.room.find(FIND_SOURCES)[0];
+                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source);
+                }
             }
         } else {
             site = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
