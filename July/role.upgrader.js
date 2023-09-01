a84eaa38500b9a8844ba1a06f98a106a2ea4b6b0
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-01 23:50:04 +0200

    opti upgrafers

--- July/role.upgrader.js
+++ July/role.upgrader.js
@@ -7,10 +7,13 @@ roleUpgrader = {
             }
         } else {
             var container = creep.pos.findClosestByPath(FIND_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_CONTAINER });
+            var resource = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
             if (container) {
                 if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(container);
                 }
+            } else if (resource) {
+                creep.pickup(resource) == ERR_NOT_IN_RANGE ? creep.moveTo(resource) : creep.pickup(resource)
             } else {
                 var source = creep.pos.findClosestByPath(FIND_SOURCES);
                 if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
