ba7ca71429736106d556b7b8388f0caa4122edad
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-01 16:03:06 +0200

    no containers upgraders

--- July/role.upgrader.js
+++ July/role.upgrader.js
@@ -7,8 +7,15 @@ roleUpgrader = {
             }
         } else {
             var container = creep.pos.findClosestByPath(FIND_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_CONTAINER });
-            if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(container);
+            if (container) {
+                if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(container);
+                }
+            } else {
+                var source = creep.pos.findClosestByPath(FIND_SOURCES);
+                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source)
+                }
             }
         }
     }
