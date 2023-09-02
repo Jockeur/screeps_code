bcc7a094a721fbc4b24a7cac5111202a83ba9de6
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-02 22:37:40 +0200

    fixed once again

--- July/role.mineralLorry.js
+++ July/role.mineralLorry.js
@@ -4,12 +4,12 @@ var role = {
 
         if (creep.store[mineralType] == creep.store.getCapacity(mineralType) && !creep.memory.working) {
             creep.memory.working = true;
-        } else if(creep.store[mineralType] == 0 && creep.memory.working){
+        } else if (creep.store[mineralType] == 0 && creep.memory.working) {
             creep.memory.working = false
         }
 
         if (!creep.memory.working) {
-            var container = creep.pos.find(FIND_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[mineralType] > 0 });
+            var container = creep.pos.findClosestByPath(FIND_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[mineralType] > 0 });
             if (container) {
                 if (creep.withdraw(container, mineralType) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(container);
