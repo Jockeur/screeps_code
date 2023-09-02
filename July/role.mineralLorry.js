003bc4db0468fdeebc9b91f7e60e1dd9a0472851
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-02 22:36:12 +0200

    mineralLorries

--- July/role.mineralLorry.js
+++ July/role.mineralLorry.js
@@ -1,22 +1,23 @@
 var role = {
     run: function (creep) {
+        const mineralType = creep.memory.mineralType
 
-        if(creep.store[creep.memory.resource] > 0) {
-            creep.memory.working = true
-        } else {
+        if (creep.store[mineralType] == creep.store.getCapacity(mineralType) && !creep.memory.working) {
+            creep.memory.working = true;
+        } else if(creep.store[mineralType] == 0 && creep.memory.working){
             creep.memory.working = false
         }
 
         if (!creep.memory.working) {
-            var container = creep.pos.find(FIND_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[creep.memory.resource] > 0 });
+            var container = creep.pos.find(FIND_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[mineralType] > 0 });
             if (container) {
-                if (creep.withdraw(container, RESOURCE_GHODIUM_OXIDE) == ERR_NOT_IN_RANGE) {
+                if (creep.withdraw(container, mineralType) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(container);
                 }
             }
         } else {
             const storage = creep.room.find(FIND_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_STORAGE });
-            if (creep.transfer(storage, RESOURCE_GHODIUM_OXIDE) == ERR_NOT_IN_RANGE) {
+            if (creep.transfer(storage, mineralType) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(storage);
             }
         }
