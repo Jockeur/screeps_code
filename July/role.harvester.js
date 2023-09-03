4e5cfbdd9fd8bd2c44105dae890ca4485a4c587e
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-03 21:52:59 +0200

    room2 harvesters

--- July/role.harvester.js
+++ July/role.harvester.js
@@ -2,7 +2,7 @@ var roleBuilder = require('role.builder');
 roleHarvester = {
     run: function (creep) {
         if (!creep.memory.working) {
-            var sources = creep.room.find(FIND_SOURCES);
+            var sources = creep.room.find(FIND_SOURCES_ACTIVE);
             if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[1]);
             }
