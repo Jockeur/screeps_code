3ab38b873f6067d5c2cdd3eace4c120fd455f7f6
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-04 18:46:00 +0200

    correct

--- July/main.js
+++ July/main.js
@@ -183,11 +183,11 @@ module.exports.loop = function () {
             if (spawn.room.terminal.store[RESOURCE_ENERGY] >= 2000 && spawn.room.terminal.store[RESOURCE_HYDROGEN] >= 2000) {
                 var orders = Game.market.getAllOrders(order => order.resourceType == RESOURCE_HYDROGEN &&
                     order.type == ORDER_BUY &&
-                    Game.market.calcTransactionCost(200, spawn.room.name, order.roomName) == 400);
+                    Game.market.calcTransactionCost(200, spawn.room.name, order.roomName) < 400);
                 console.log('Hydrogen buy orders found: ' + orders.length);
                 orders.sort(function (a, b) { return b.price - a.price; });
                 console.log('Best price: ' + orders[0].price);
-                if (orders[0].price == 0.7) {
+                if (orders[0].price > 0.7) {
                     var result = Game.market.deal(orders[0].id, 200, spawn.room.name);
                     if (result == 0) {
                         console.log('Order completed successfully');
