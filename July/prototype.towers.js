c0ef36c79a953476d6446fc2336bd3a385ae67a0
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-05 17:08:27 +0200

    fixing

--- July/prototype.towers.js
+++ July/prototype.towers.js
@@ -1,12 +1,12 @@
 StructureTower.prototype.defend =
     function () {
-        var target = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        var target = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if (target) {
-            tower.attack(target);
+            this.attack(target);
         }
 
-        var heal = tower.pos.findClosestByPath(FIND_MY_CREEPS, { filter: (c) => c.hits < c.hitsMax });
+        var heal = this.pos.findClosestByPath(FIND_MY_CREEPS, { filter: (c) => c.hits < c.hitsMax });
         if (!target && heal) {
-            tower.heal(heal);
+            this.heal(heal);
         }
     }
