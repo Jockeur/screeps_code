014c46233af5fa8fe8420cf9517581ac42b915de
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-11 18:47:59 +0200

    not spawn. THIS

--- July/prototype.terminal.js
+++ July/prototype.terminal.js
@@ -9,15 +9,15 @@ StructureTerminal.prototype.sell =
             case RESOURCE_LEMERGIUM: resource = RESOURCE_LEMERGIUM_BAR;
         }
 
-        if (spawn.room.terminal.store[RESOURCE_ENERGY] >= 2000 && spawn.room.terminal.store[resource] >= 2000) {
+        if (this.store[RESOURCE_ENERGY] >= 2000 && this.store[resource] >= 2000) {
             var orders = Game.market.getAllOrders(order => order.resourceType == resource &&
                 order.type == ORDER_BUY &&
-                Game.market.calcTransactionCost(200, spawn.room.name, order.roomName) < 400);
+                Game.market.calcTransactionCost(200, this.room.name, order.roomName) < 400);
             console.log(resource + ' buy orders found: ' + orders.length);
             orders.sort(function (a, b) { return b.price - a.price; });
             console.log('Best price: ' + orders[0].price);
             if (orders[0].price > 0.7) {
-                var result = Game.market.deal(orders[0].id, 200, spawn.room.name);
+                var result = Game.market.deal(orders[0].id, 200, this.room.name);
                 if (result == 0) {
                     console.log('Order completed successfully');
                 }
