be6c2aae268378d249af9511e694dd0a68f7c555
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-11 17:57:44 +0200

    factory

--- July/prototype.factory.js
+++ July/prototype.factory.js
@@ -1,10 +1,8 @@
 StructureFactory.prototype.compact =
     function (resourceType) {
-        if (this.store.energy > 1000 && this.store[resourceType] > 2000) {
-            switch (resourceType) {
-                case RESOURCE_HYDROGEN: this.produce(RESOURCE_REDUCTANT);
-                case RESOURCE_OXYGEN: this.produce(RESOURCE_OXIDANT);
-                case RESOURCE_LEMERGIUM: this.produce(RESOURCE_LEMERGIUM_BAR);
-            }
+        switch (resourceType) {
+            case RESOURCE_HYDROGEN: this.produce(RESOURCE_REDUCTANT);
+            case RESOURCE_OXYGEN: this.produce(RESOURCE_OXIDANT);
+            case RESOURCE_LEMERGIUM: this.produce(RESOURCE_LEMERGIUM_BAR);
         }
     }
