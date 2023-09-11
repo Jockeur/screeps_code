125b1dcd5375a31b123b6e61cebf92e5d98b73da
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-11 19:03:17 +0200

    fixed dealer

--- July/role.dealer.js
+++ July/role.dealer.js
@@ -1,7 +1,13 @@
 module.exports = {
     run: function (creep) {
         const mineralType = creep.memory.mineralType;
-        const target = creep.memory.target
+        const target = creep.memory.target;
+        var resource;
+        switch (mineralType) {
+            case RESOURCE_HYDROGEN: resource = RESOURCE_REDUCTANT; break;
+            case RESOURCE_OXYGEN: resource = RESOURCE_OXIDANT; break;
+            case RESOURCE_LEMERGIUM: resource = RESOURCE_LEMERGIUM_BAR; break;
+        }
 
         if (creep.store[mineralType] == creep.store.getCapacity(mineralType) && !creep.memory.working) {
             creep.memory.working = true;
@@ -10,18 +16,13 @@ module.exports = {
         }
 
         if(creep.memory.working) {
-            if(creep.transfer(mineralType, creep.room.terminal) == ERR_NOT_IN_RANGE) {
+            if(creep.transfer(creep.room.terminal, resource) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.terminal);
             }
         } else {
-            var resource;
+            
             const factory = creep.room.find(FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_FACTORY})[0];
-            switch (mineralType) {
-                case RESOURCE_HYDROGEN: resource = RESOURCE_REDUCTANT;
-                case RESOURCE_OXIGEN: resource = RESOURCE_OXIDANT;
-                case RESOURCE_LEMERGIUM: resource = RESOURCE_LEMERGIUM_BAR;
-            }
-            if(creep.withdraw(resource, factory) == ERR_NOT_IN_RANGE) {
+            if(creep.withdraw(factory, resource) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(factory);
             }
         }
