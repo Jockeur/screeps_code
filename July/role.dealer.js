49d2e1dc8537dda7e74d6673af7cd880be35273c
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-11 18:24:16 +0200

    dealers

--- July/role.dealer.js
+++ July/role.dealer.js
@@ -8,5 +8,22 @@ module.exports = {
         } else if (creep.store[mineralType] == 0 && creep.memory.working) {
             creep.memory.working = false
         }
+
+        if(creep.memory.working) {
+            if(creep.transfer(mineralType, creep.room.terminal) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.terminal);
+            }
+        } else {
+            var resource;
+            const factory = creep.room.find(FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_FACTORY})[0];
+            switch (mineralType) {
+                case RESOURCE_HYDROGEN: resource = RESOURCE_REDUCTANT;
+                case RESOURCE_OXIGEN: resource = RESOURCE_OXIDANT;
+                case RESOURCE_LEMERGIUM: resource = RESOURCE_LEMERGIUM_BAR;
+            }
+            if(creep.withdraw(resource, factory) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(factory);
+            }
+        }
     }
 };
