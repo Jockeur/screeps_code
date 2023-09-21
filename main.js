eed48e857cf83ca09df4ebe687ec444153b12fd7
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-21 13:11:22 +0200

    changing a little bit

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ module.exports.loop = function () {
 
         for (link of links){
             const id = link.id
-            Memory.rooms[HOME].structures.links[id] = {pos: link.pos, state: undefined};
+            Memory.rooms[HOME].structures.links[id] = {pos: link.pos, state: '', target: ''};
         }
 
         if(factory) {
