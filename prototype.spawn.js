cf1f093ad91d418312cd5f827206cd5c9571d335
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-22 15:12:47 +0200

    linkers spawning

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -1,4 +1,4 @@
-var listOfRoles = ['harvester', 'lorry', 'dealer', 'claimer', 'upgrader', 'repairer', 'factorer', 'builder', 'wallRepairer', 'miner', 'excavator', 'mineralLorry'];
+var listOfRoles = ['harvester', 'lorry', 'dealer', 'claimer', 'upgrader', 'repairer', 'factorer', 'builder', 'wallRepairer', 'miner', 'excavator', 'mineralLorry', 'linker'];
 var listOfLDC = ['localBuilder'];
 
 StructureSpawn.prototype.spawnCreepsIfNecessary =
