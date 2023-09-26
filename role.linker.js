8f746d2aed57e9094c4f3efe356e695d780f50a0
Jockeur <74934764+Jockeur@users.noreply.github.com>
2023-09-27 00:18:31 +0200

    ok

--- role.linker.js
+++ role.linker.js
@@ -1,7 +1,7 @@
 module.exports = {
     run: function(creep) {
         if(creep.memory.working) {
-            const link = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_LINK && s.store.getFreeCapacity() > 0});
+            const link = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_LINK && s.store.getFreeCapacity(RESOURCE_ENERGY) > 0});
             if(creep.transfer(link, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(link);
             }
